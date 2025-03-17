2511826dbaa385228c9e8a793e417b346274bd9f
𓆰𝅃꯭᳚𓄂️𝆺𝅥⃝🔥 ⃪ͥ͢ ᷟ𓆩 𝛭꯭ʀ꯭ 𝗛꯭ᴏ꯭ɴ꯭ᴇ꯭ʏ꯭ 乛|⁪⁬⁮⁮⁮⁮ ‌⁪⁬𓆪🐼™ 𝆺꯭𝅥˶꯭꯭꯭֟፝͟͢͝⏎͟›͢ <Mdhakeeb07302@gmail.com>
2025-03-17 20:07:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,8 +42,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/MEOW_NETWORK_OP")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/MEOW_NETWORK_OP")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/llVAMPIRE_UPDATEll")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/llVAMPIRE_UPDATEll")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
